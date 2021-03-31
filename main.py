300b4d376adf4c9ec8d414c8aba7d745a9618b88
Audrey Andoy <audreylandoy@gmail.com>
2021-03-31 06:01:49 -0700

    class template to distribute

---
+++ main.py
@@ -0,0 +1,37 @@
+
+first instance
+quinn = Student(
+                "Quinn", 
+                "junior", 
+                [
+                    "Pre-Calc", 
+                    "English III", 
+                    "World History", 
+                    "Gym", 
+                    "Chemistry", 
+                    "Music Composition"
+                ]
+            )
+
+quinn.add_class("Painting")
+quinn.get_num_classes()
+quinn.summary()
+
+claire = Student(
+                "Claire", 
+                "freshmen", 
+                [
+                    "Algebra", 
+                    "Writing", 
+                    "Contemporary Issues", 
+                    "Gym", 
+                    "Earth Science", 
+                    "Painting"
+                ]
+            )
+
+claire.get_num_classes()
+claire.summary()
+
+get_student_with_more_classes(claire, samara)
+
